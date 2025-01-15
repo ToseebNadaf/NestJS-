6a53276f1b434f49d3521283a11226d93d4a4389
ToseebNadaf <toseebnadaf@gmail.com>
2025-01-15 15:43:57 +0530

    Implement DTO validation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,50 +7,42 @@ import {
   Patch,
   Post,
   Query,
+  ParseIntPipe,
+  ValidationPipe,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Get()
-  findAll(@Query('role') role?: 'ADMIN' | 'INTERN' | 'ENGINEER') {
+  @Get() // GET /users or /users?role=value
+  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
     return this.usersService.findAll(role);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+  @Get(':id') // GET /users/:id
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.findOne(id);
   }
 
-  @Post()
-  create(
-    @Body()
-    user: {
-      name: string;
-      email: string;
-      role: 'ADMIN' | 'INTERN' | 'ENGINEER';
-    },
-  ) {
-    return this.usersService.create(user);
+  @Post() // POST /users
+  create(@Body(ValidationPipe) createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
   }
 
-  @Patch(':id')
+  @Patch(':id') // PATCH /users/:id
   update(
-    @Param('id') id: string,
-    @Body()
-    userUpdate: {
-      name?: string;
-      email?: string;
-      role?: 'INTERN' | 'ENGINEER' | 'ADMIN';
-    },
+    @Param('id', ParseIntPipe) id: number,
+    @Body(ValidationPipe) updateUserDto: UpdateUserDto,
   ) {
-    return this.usersService.update(+id, userUpdate);
+    return this.usersService.update(id, updateUserDto);
   }
 
-  @Delete(':id')
-  delete(@Param('id') id: string) {
-    return this.usersService.delete(+id);
+  @Delete(':id') // DELETE /users/:id
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.delete(id);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class UsersService {
@@ -35,46 +38,38 @@ export class UsersService {
     },
   ];
 
-  findAll(role?: 'ADMIN' | 'ENGINEER' | 'INTERN') {
+  findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
     if (role) {
-      return this.users.filter((user) => user.role == role);
+      const rolesArray = this.users.filter((user) => user.role === role);
+      if (rolesArray.length === 0)
+        throw new NotFoundException('User Role Not Found');
+      return rolesArray;
     }
     return this.users;
   }
 
   findOne(id: number) {
-    const user = this.users.find((user) => user.id == id);
+    const user = this.users.find((user) => user.id === id);
+
+    if (!user) throw new NotFoundException('User Not Found');
+
     return user;
   }
 
-  create(user: {
-    name: string;
-    email: string;
-    role: 'ADMIN' | 'INTERN' | 'ENGINEER';
-  }) {
-    const userByHighId = [...this.users].sort(
-      (valueA, valueB) => valueB.id - valueA.id,
-    );
+  create(createUserDto: CreateUserDto) {
+    const usersByHighestId = [...this.users].sort((a, b) => b.id - a.id);
     const newUser = {
-      id: userByHighId[0].id + 1,
-      ...user,
+      id: usersByHighestId[0].id + 1,
+      ...createUserDto,
     };
-
     this.users.push(newUser);
     return newUser;
   }
 
-  update(
-    id: number,
-    userUpdate: {
-      name?: string;
-      email?: string;
-      role?: 'INTERN' | 'ENGINEER' | 'ADMIN';
-    },
-  ) {
+  update(id: number, updateUserDto: UpdateUserDto) {
     this.users = this.users.map((user) => {
       if (user.id === id) {
-        return { ...user, ...userUpdate };
+        return { ...user, ...updateUserDto };
       }
       return user;
     });
